23fcf73ee86e1d95e467f163445b9f0567a4cc4e
fegge <email@fredrikdahlgren.com>
2019-09-05 02:50:15 -0700

    Added test for problem 10 from set 2.

--- tests/set_2.rs
+++ tests/set_2.rs
@@ -1,6 +1,12 @@
 pub mod set_2 {
-    use cryptopals::crypto::symmetric::padding_modes::{PaddingMode, Pkcs7};
-    
+    use cryptopals::crypto::symmetric;
+
+    use symmetric::padding_modes::{PaddingMode, Pkcs7};
+    use symmetric::cipher_modes::{CipherMode, Cbc};
+    use symmetric::ciphers::{Cipher, Aes128};
+
+    type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+
     #[test]
     fn problem_9() {
         let pkcs7 = Pkcs7::new(20);
@@ -11,5 +17,21 @@ pub mod set_2 {
         let result = pkcs7.pad_buffer(&mut buffer, 16);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), "YELLOW SUBMARINE\x04\x04\x04\x04".as_bytes());
+    }
+
+    #[test]
+    fn problem_10() {
+        let mut buffer = include_bytes!("../data/set_2/problem_10.bin").to_owned();
+        let key = "YELLOW SUBMARINE".as_bytes();
+        let iv = [0; Aes128::BLOCK_SIZE];
+        let mut cipher = Aes128Cbc::new(key, &iv).unwrap();
+
+        let result = cipher.decrypt_buffer(&mut buffer[..]);
+        assert!(result.is_ok());
+
+        let string = std::str::from_utf8(result.unwrap());
+        assert!(string.is_ok());
+
+        println!("{}", string.unwrap());
     }
 }
