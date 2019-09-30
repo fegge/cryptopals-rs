acb09f935820be6a778fd8d3cfcabddf1e40e698
fegge <email@fredrikdahlgren.com>
2019-09-30 12:14:02 -0700

    Added block size detection to simple_ecb_decryption module.

--- src/attacks/symmetric.rs
+++ src/attacks/symmetric.rs
@@ -27,35 +27,51 @@ pub mod ecb_cbc_detection {
 
 pub mod simple_ecb_decryption {
     use crate::crypto::symmetric::Error;
-    use crate::crypto::symmetric::ciphers::{Cipher, Aes128};
 
-    fn get_known_data(size: usize) -> Vec<u8> {
-        let mut result = Vec::with_capacity(Aes128::BLOCK_SIZE);
-        result.resize(Aes128::BLOCK_SIZE - (size % Aes128::BLOCK_SIZE) - 1, 0);
+    fn get_known_data(suffix_size: usize, block_size: usize) -> Vec<u8> {
+        let mut result = Vec::with_capacity(block_size);
+        result.resize(block_size - (suffix_size % block_size) - 1, 0);
         result
     }
 
-    fn get_known_data_with_suffix(suffix: &[u8]) -> Vec<u8> {
-        let mut result = get_known_data(suffix.len());
+    fn get_known_data_with_suffix(suffix: &[u8], block_size: usize) -> Vec<u8> {
+        let mut result = get_known_data(suffix.len(), block_size);
         result.extend(suffix);
         result
     }
 
-    pub fn get_unknown_data<Oracle>(unknown_size: usize, mut encrypt_buffer: Oracle)
-        -> Result<Vec<u8>, Error> where Oracle: FnMut(&[u8]) -> Result<Vec<u8>, Error>
+    pub fn get_block_size<Oracle>(mut encrypt_buffer: Oracle) -> Result<usize, Error> 
+        where Oracle: FnMut(&[u8]) -> Result<Vec<u8>, Error> 
     {
+        for block_size in 8..=256 {
+            let result = encrypt_buffer(&vec![0; 2 * block_size])?;
+            let mut blocks = result.chunks(block_size);
+            if blocks.next() == blocks.next() {
+                // Since the input is padded, the first block will always be Some(data).
+                return Ok(block_size);
+            }
+        }
+        Err(Error::CipherError)
+    }
+
+    pub fn get_unknown_data<Oracle>(unknown_size: usize, mut encrypt_buffer: Oracle) -> Result<Vec<u8>, Error> 
+        where Oracle: FnMut(&[u8]) -> Result<Vec<u8>, Error> 
+    {
+        println!("{:?}", get_block_size(|buffer| encrypt_buffer(buffer)));
+        let block_size = get_block_size(|buffer| encrypt_buffer(buffer))?;
+        
         let mut unknown_data = Vec::new();
         while unknown_data.len() < unknown_size {
-            let mut known_data = get_known_data(unknown_data.len());
+            let mut known_data = get_known_data(unknown_data.len(), block_size);
             let target_data = encrypt_buffer(&known_data)?;
             
-            known_data = get_known_data_with_suffix(&unknown_data);
+            known_data = get_known_data_with_suffix(&unknown_data, block_size);
             let mut last_byte = 0;
             known_data.push(last_byte);
             let mut test_data = encrypt_buffer(&known_data)?;
             
-            let begin = Aes128::BLOCK_SIZE * (unknown_data.len() / Aes128::BLOCK_SIZE);
-            let end = begin + Aes128::BLOCK_SIZE;
+            let begin = block_size * (unknown_data.len() / block_size);
+            let end = begin + block_size;
             while test_data[begin..end] != target_data[begin..end] {           
                 if last_byte == 255 {
                     // Note that this is not an error state. This will in fact
