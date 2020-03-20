eea962b38a3b68aa04ebf7b6f935b0a6fbdd2830
fegge <email@fredrikdahlgren.com>
2020-03-20 20:36:49 +0100

    Added solution for problem 5 from set 1.

--- src/attacks/statistics.rs
+++ src/attacks/statistics.rs
@@ -18,42 +18,43 @@ pub mod single_byte_xor {
     }
 
     // English lowercase monogram statistics.
-    fn get_monogram_statistics() -> Distribution<u8> {
+    pub fn get_monogram_statistics() -> Distribution<u8> {
         dist!(
-            b'a' => 8.167,
-            b'b' => 1.492,
-            b'c' => 2.202,
-            b'd' => 4.253,
-            b'e' => 12.702,
-            b'f' => 2.228,
-            b'g' => 2.015,
-            b'h' => 6.094,
-            b'i' => 6.966,
-            b'j' => 0.153,
-            b'k' => 1.292,
-            b'l' => 4.025,
-            b'm' => 2.406,
-            b'n' => 6.749,
-            b'o' => 7.507,
-            b'p' => 1.929,
-            b'q' => 0.095,
-            b'r' => 5.987,
-            b's' => 6.327,
-            b't' => 9.356,
-            b'u' => 2.758,
-            b'v' => 0.978,
-            b'w' => 2.560,
-            b'x' => 0.150, 
-            b'y' => 1.994,
-            b'z' => 0.077
+            b'a' => 0.065_173_8,
+            b'b' => 0.012_424_8,
+            b'c' => 0.021_733_9,
+            b'd' => 0.034_983_5,
+            b'e' => 0.104_144_2,
+            b'f' => 0.019_788_1,
+            b'g' => 0.015_861_0,
+            b'h' => 0.049_288_8,
+            b'i' => 0.055_809_4,
+            b'j' => 0.000_903_3,
+            b'k' => 0.005_052_9,
+            b'l' => 0.033_149_0,
+            b'm' => 0.020_212_4,
+            b'n' => 0.056_451_3,
+            b'o' => 0.059_630_2,
+            b'p' => 0.013_764_5,
+            b'q' => 0.000_860_6,
+            b'r' => 0.049_756_3,
+            b's' => 0.051_576_0,
+            b't' => 0.072_935_7,
+            b'u' => 0.022_513_4,
+            b'v' => 0.008_290_3,
+            b'w' => 0.017_127_2,
+            b'x' => 0.001_369_2,
+            b'y' => 0.014_598_4,
+            b'x' => 0.000_783_6,
+            b' ' => 0.191_818_2
         )
     }
 
-    fn decrypt(key: u8, ciphertext: &[u8]) -> Vec<u8> {
+    pub fn decrypt_ciphertext(key: u8, ciphertext: &[u8]) -> Vec<u8> {
         ciphertext.iter().map(|byte| key ^ byte).collect::<Vec<u8>>()
     }
 
-    fn score(plaintext: &[u8], distribution: &Distribution<u8>) -> f64 {
+    pub fn score_plaintext(plaintext: &[u8], distribution: &Distribution<u8>) -> f64 {
         plaintext
             .iter()
             .collect::<Distribution<u8>>()
@@ -64,11 +65,11 @@ pub mod single_byte_xor {
         let distribution = get_monogram_statistics();
         let result = (0..=255)
             .map(|key|
-                decrypt(key, &ciphertext)
+                decrypt_ciphertext(key, &ciphertext)
             )
             .minimize(|plaintext|
                 // We should really convert the plaintext to lowercase before scoring, but YOLO.
-                score(&plaintext, &distribution)
+                score_plaintext(&plaintext, &distribution)
             );
        
         Ok(String::from_utf8(result.0)?)
@@ -81,7 +82,7 @@ pub mod detect_single_byte_xor {
     use crate::math::optimization::Minimize;
     use crate::math::statistics::Distribution;
 
-    fn score(ciphertext: &[u8]) -> f64 {
+    fn score_ciphertext(ciphertext: &[u8])  -> f64 {
         ciphertext
             .iter()
             .collect::<Distribution<u8>>()
@@ -92,8 +93,68 @@ pub mod detect_single_byte_xor {
         let result = ciphertexts
             .iter()
             .minimize(|ciphertext|
-                score(ciphertext)
+                score_ciphertext(ciphertext)
             );
         single_byte_xor::recover_plaintext(result.0)
+    }
+}
+
+pub mod repeating_key_xor {
+    use super::single_byte_xor;
+    use crate::math::optimization::Minimize;
+    use crate::math::statistics::Distribution;
+    
+    #[derive(Debug)]
+    pub enum Error {
+        RecoveryError,
+    }
+
+    fn hamming_distance(lhs: &[u8], rhs: &[u8]) -> u32 {
+        lhs.iter().zip(rhs)
+            .fold(0, |sum, (x, y)| sum + (x ^ y).count_ones())
+    }
+
+    /// Returns the average hamming distance per byte for the given key size.
+    fn score_key_size(key_size: usize, ciphertext: &[u8]) -> f64 {
+        let mut sum = 0;
+        let mut total = 0;
+        for (i, lhs) in ciphertext.chunks(key_size).enumerate() {
+            for (j, rhs) in ciphertext.chunks(key_size).enumerate() {
+                if i < j {
+                    sum += hamming_distance(lhs, rhs);
+                    total += 1;
+                }
+            }
+        }
+        (sum as f64) / ((total * key_size) as f64)
+    }
+
+    fn recover_key_byte(ciphertext: &[u8], distribution: &Distribution<u8>) -> u8 {
+        (0..=255).minimize(|&key|
+            single_byte_xor::decrypt_ciphertext(key, ciphertext)
+                .iter()
+                .collect::<Distribution<u8>>()
+                .distance_from(distribution)
+            ).0
+    }
+
+    pub fn recover_plaintext(ciphertext: &[u8]) -> Result<String, Error> {
+        let key_size = (1..40).minimize(|&key_size|
+            score_key_size(key_size, ciphertext)
+        ).0;
+
+        let mut key = Vec::new();
+        let distribution = single_byte_xor::get_monogram_statistics();
+        for offset in 0..key_size {
+            let bytes: Vec<u8> = ciphertext
+                .iter()
+                .skip(offset)
+                .step_by(key_size)
+                .cloned()
+                .collect();
+            key.push(recover_key_byte(&bytes, &distribution));
+        }
+        println!("{:?}", String::from_utf8(key));
+        Err(Error::RecoveryError)
     }
 }
--- tests/set_1.rs
+++ tests/set_1.rs
@@ -62,4 +62,37 @@ mod set_1 {
             assert_eq!(result.unwrap(), "Now that the party is jumping\n");
         }
     }
+
+    mod problem_5 {
+        use hex;
+        use cryptopals::crypto::symmetric::{StreamCipherMode, Xor};
+        
+        #[test]
+        fn solution() {
+            let ciphertext = hex::decode(
+                "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272\
+                 a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+            ).unwrap();
+            let result = Xor::new("ICE".as_bytes()) .decrypt_str(&ciphertext);
+
+            assert_eq!(
+                result.unwrap(), 
+                "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+            );
+        }
+    }
+
+    mod problem_6 {
+        use base64;
+        use cryptopals::attacks::statistics::repeating_key_xor;
+
+        #[test]
+        fn solution() {
+            let ciphertext = base64::decode(
+                &include_str!("../data/set_1/problem_6.txt").replace("\n", "")
+            ).unwrap();
+            
+            repeating_key_xor::recover_plaintext(&ciphertext);
+        }
+    }
 }
